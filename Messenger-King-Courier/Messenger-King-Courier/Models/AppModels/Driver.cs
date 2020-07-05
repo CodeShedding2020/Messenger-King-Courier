1026440c409be5501b538c67a140498ce2ba90ca
Precedence <precedencemthethwaphiwe@gmail.com>
2020-07-05 09:28:38 -0700

    Classes Complete

--- Messenger-King-Courier/Messenger-King-Courier/Models/AppModels/Driver.cs
+++ Messenger-King-Courier/Messenger-King-Courier/Models/AppModels/Driver.cs
@@ -13,33 +13,31 @@ namespace Messenger_King_Courier.Models.AppModels
         [Required(ErrorMessage = "ID number is required")]
         public string Driver_IDNo { get; set; }
 
-        [Required(ErrorMessage = "Driver name is required")]
+        [Required(ErrorMessage = "First name is required")]
         [DisplayName("First name")]
         public string Driver_Name { get; set; }
 
-        [Required(ErrorMessage = "Driver last name is required")]
+        [Required(ErrorMessage = "Last name is required")]
         [DisplayName("Last name ")]
         public string Driver_Surname { get; set; }
 
-        [Required(ErrorMessage = "Vehicle model is required")]
-        [DisplayName("Mobile number")]
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Contact Number")]
+        [Required(ErrorMessage = "Contact number is required!")]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$",
+                  ErrorMessage = "Entered Contact number format is not valid.")]
         public string Driver_CellNo { get; set; }
 
-        [Required(ErrorMessage = "driver residence address is required")]
+        [Required(ErrorMessage = "Residence address is required")]
         [DisplayName("Residence address")]
         public string Driver_Address { get; set; }
 
-        [Required(ErrorMessage = "driver residence address is required")]
+        [Required(ErrorMessage = "Email address is required")]
         [DisplayName("Email address")]
         [EmailAddress]
         public string Driver_Email { get; set; }
 
-        [DataType(DataType.PhoneNumber)]
-        [Display(Name = "Contact Number")]
-        [Required(ErrorMessage = "Contact Number Required!")]
-        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$",
-                  ErrorMessage = "Entered Contact number format is not valid.")]
-        public string Driver_Contact { get; set; }
+
         public virtual List<Document> Documents { get; set;}
 
         public virtual List<Bank> Banks { get; set; }
