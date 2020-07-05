43cb032d862508c2054d59e7150f958b71eab376
Rodwin Govender <21618914@dut4life.ac.za>
2020-07-06 00:56:49 +0200

    Scaffolded controllers and views for Vehicle and Driver

---
+++ Messenger-King-Courier/Messenger-King-Courier/Models/DriverDetails.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Driver.Models
+{
+    public class DriverDetails
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Driver_ID { get; set; }
+
+        [StringLength(40, MinimumLength = 2, ErrorMessage = "Name should be between 2 to 40 Characters")]
+        [Required(ErrorMessage = "The Name of the Driver is required")]
+        [DisplayName("Driver Name")]
+        public string Driver_Name { get; set; }
+
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Surname should be between 2 to 50 Characters")]
+        [Required(ErrorMessage = "The Surname of the Driver is required")]
+        [DisplayName("Driver Surname")]
+        public string Driver_Surname { get; set; }
+
+        [StringLength(13, MinimumLength = 13, ErrorMessage = "Please enter a valid SA Id Number")]
+        [Required(ErrorMessage = "The Id Number of the Driver is required")]
+        [DisplayName("ID number")]
+        public string Driver_IDNO { get; set; }
+
+        [StringLength(15, MinimumLength = 10, ErrorMessage = "Please enter a valid phone number")]
+        [Required(ErrorMessage = "The Cell number of the Driver is required")]
+        [DisplayName("Cell Number")]
+        public string Driver_Cell { get; set; }
+
+        [Required(ErrorMessage = "An Address is required")]
+        [DisplayName("Address")]
+        public int Driver_Address { get; set; }                                                     
+
+    }
+}
---
+++ Messenger-King-Courier/Messenger-King-Courier/Models/VehicleDetails.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Driver.Models
+{
+    public class VehicleDetails
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Vehicle_ID_VIN { get; set; }
+
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Make should be between 2 to 20 Characters")]
+        [Required(ErrorMessage = "The Make is required")]
+        [DisplayName("Make")]
+        public string Make { get; set; }
+
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Model should be between 2 to 20 Characters")]
+        [Required(ErrorMessage = "The Model is required")]
+        [DisplayName("Model")]
+        public string Model { get; set; }
+
+        [StringLength(4, MinimumLength = 4, ErrorMessage = "Please enter a valid Year")]
+        [Required(ErrorMessage = "The Year is required")]
+        [DisplayName("Year")]
+        public string Year { get; set; }
+
+        
+        [Required(ErrorMessage = "The Registration number is required")]
+        [DisplayName("Registration Number")]
+        public string Reg_No { get; set; }
+
+        [Required(ErrorMessage = "A colour is required")]
+        [DisplayName("Colour")]
+        public int Colour { get; set; }
+
+    }
+}
