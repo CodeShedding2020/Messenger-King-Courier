305c37a1784b4371a1c1b5e529f06192a9d2b351
Dhirvan <dhirvanj@gmail.com>
2020-07-06 18:13:10 +0200

    I added imvoices and months, hope everything is good

---
+++ Messenger-King-Courier/Messenger-King-Courier/Controllers/MonthsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Messenger_King_Courier.Models;
+using Messenger_King_Courier.Models.AppModels;
+
+namespace Messenger_King_Courier.Controllers
+{
+    public class MonthsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Months
+        public ActionResult Index()
+        {
+            return View(db.Months.ToList());
+        }
+
+        // GET: Months/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Month month = db.Months.Find(id);
+            if (month == null)
+            {
+                return HttpNotFound();
+            }
+            return View(month);
+        }
+
+        // GET: Months/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Months/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Month_ID,Month_Name")] Month month)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Months.Add(month);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(month);
+        }
+
+        // GET: Months/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Month month = db.Months.Find(id);
+            if (month == null)
+            {
+                return HttpNotFound();
+            }
+            return View(month);
+        }
+
+        // POST: Months/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Month_ID,Month_Name")] Month month)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(month).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(month);
+        }
+
+        // GET: Months/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Month month = db.Months.Find(id);
+            if (month == null)
+            {
+                return HttpNotFound();
+            }
+            return View(month);
+        }
+
+        // POST: Months/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Month month = db.Months.Find(id);
+            db.Months.Remove(month);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
