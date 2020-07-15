18fc0d7d13289943f9664742beafef2751f6af6a
Precedence <precedencemthethwaphiwe@gmail.com>
2020-07-15 09:08:54 -0700

    testing

--- Messenger-King-Courier/Messenger-King-Courier/Models/AppModels/Quote.cs
+++ Messenger-King-Courier/Messenger-King-Courier/Models/AppModels/Quote.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Cryptography;
 
 namespace Messenger_King_Courier.Models.AppModels
 {
